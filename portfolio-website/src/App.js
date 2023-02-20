b9765a5cb00d14d78787eec5932aea5bb4f73833
YasHabib <yasin_habib@outlook.com>
2023-02-19 23:02:20 -0700

    Added react projects

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -3,6 +3,7 @@ import About from "./pages/About";
 import Experiences from "./pages/Experiences";
 import NetCoreProjects from "./pages/NetCoreProjects";
 import Contact from "./pages/Contact";
+import ReactjsProjects from "./pages/ReactjsProjects";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const route = createBrowserRouter([
@@ -10,7 +11,8 @@ const route = createBrowserRouter([
   { path: '/about',element: <About/> },
   { path: '/projects',element: <Experiences/> },
   { path: '/.netcore',element: <NetCoreProjects/> },
-  { path: '/contact',element: <Contact/> }
+  { path: '/contact',element: <Contact/> },
+  { path: '/reactjs',element: <ReactjsProjects/> }
   ])
 
 
--- portfolio-website/src/pages/ReactjsProjects.jsx
+++ portfolio-website/src/pages/ReactjsProjects.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import MKTFY from '../assets/MKTFY.png'
-import MKTFYbackend from '../assets/E-commerce.png'
-import ChatWPF from '../assets/chat.png'
+import Portfolio from '../assets/Portfolio.png'
 
-
-function NetCoreProjects() {
+function ReactjsProjects() {
   return (
     <div name='netCoreProjects' className='w-full md:h-screen text-gray-300 bg-[#0D0C4B]'>
         <Navbar/>
@@ -13,8 +11,8 @@ function NetCoreProjects() {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='h-[80px]'></div>
             <div className='pb-8'>
-                <text className='text-4xl font-bold inline border-b-4 border-amber-700'>.NET Projects</text><br/>
-                <p className='py-6'>Check out some of my recent .NET projects. <br/>
+                <text className='text-4xl font-bold inline border-b-4 border-amber-700'>React.js</text><br/>
+                <p className='py-6'>Check out some of my recent React.js projects. <br/>
                 To look at the code, please hover over the image and click the github link</p>
             </div>
 
@@ -30,7 +28,7 @@ function NetCoreProjects() {
                             MarketForYou Frontend
                         </span>
                         <div>
-                            <a href='https://github.com/YasHabib/MarketPlace_For_You' target={'_blank'}>
+                            <a href='https://github.com/YasHabib/Market_for_you_frontend/tree/development' target={'_blank'}>
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Github</button>
                             </a>
                             {/* <a href='/'>
@@ -42,7 +40,7 @@ function NetCoreProjects() {
                 <div>
                     <p><b>Name:</b> MarketForYou Frontend
                         <br/>
-                        <b>Description:</b> A frontend application of a E-commerce site using React.js. Currently in progress and will be connected to the RESTful APIs created in MarketForYou: Backend
+                        <b>Description:</b> A frontend application of a E-commerce site using React.js. Currently in progress and will be connected to the RESTful APIs created in MarketForYou: Backend.
                     </p>
                 </div>
             </div>
@@ -51,16 +49,16 @@ function NetCoreProjects() {
             <br/><br/>
             {/* Project: WPF Chat application */}
             <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-4'>
-                <div style={{backgroundImage: `url(${ChatWPF})`}} 
+                <div style={{backgroundImage: `url(${Portfolio})`}} 
                 className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover effects */}
                     <div className=' opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
-                            Windows Platform Foundation
+                            My Portfolio
                         </span>
                         <div>
-                            <a href='https://github.com/YasHabib/ChatApp-WPF/tree/development' target={'_blank'}>
+                            <a href='https://github.com/YasHabib/yashabib.github.io' target={'_blank'}>
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Github</button>
                             </a>
                             {/* <a href='/'>
@@ -70,10 +68,9 @@ function NetCoreProjects() {
                     </div>
                 </div>
                 <div>
-                    <p><b>Name:</b> ChittyChat
+                    <p><b>Name:</b> My Portflio (this website)
                         <br/>
-                        <b>Description:</b> A Chat application using WPF.Core. No server is currently integrated or external APIs. This application currenly have some seeded data to do testing. 
-                        As a user, you can send messages and it'll be displayed on the main window. 
+                        <b>Description:</b> Developed using React.js and Tailwind. React-router-dom was used to give it a multipage feel while keeping the advantages of a Single Page Application. GetForm was used to develop the contact form.
                     </p>
                 </div>
             </div>
@@ -86,4 +83,4 @@ function NetCoreProjects() {
   )
 }
 
-export default NetCoreProjects
+export default ReactjsProjects
